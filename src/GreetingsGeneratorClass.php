fc362aa0def15d16455a88642f8287aadf40ae3c
Syed Rizul Islam Tanay <104726896+tanayweb@users.noreply.github.com>
2025-01-22 23:46:40 +0600

    Package main structure has been made.

---
+++ src/GreetingsGeneratorClass.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Tanayweb\GreetingsGenerator;
+
+class GreetingsGeneratorClass {}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Tanayweb\GreetingsGenerator\Tests;
 
 use PHPUnit\Framework\TestCase;
 
