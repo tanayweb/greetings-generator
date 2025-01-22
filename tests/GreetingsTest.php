6f908aed99694ac7503ea4fee15cbae31d55276a
Syed Rizul Islam Tanay <104726896+tanayweb@users.noreply.github.com>
2025-01-23 00:35:02 +0600

    Unit testing done

--- tests/GreetingsTest.php
+++ tests/GreetingsTest.php
@@ -5,9 +5,11 @@ namespace Tanayweb\GreetingsGenerator\Tests;
 use PHPUnit\Framework\TestCase;
 use Tanayweb\GreetingsGenerator\Greetings;
 
-class GreetingsTest extends TestCase {
-    public function test_it_generates_random_greeting(){
-        $messages = ['Hi','Hello','How are you'];
-        $this->assertContains(Greetings::get_message(),$messages);
+class GreetingsTest extends TestCase
+{
+    public function test_it_generates_random_greeting()
+    {
+        $messages = ['Hi', 'Hello', 'How are you'];
+        $this->assertContains(Greetings::get_message(), $messages);
     }
 }
